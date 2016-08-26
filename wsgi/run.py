df6717dfb0ee2d2d3c8e0a3485644061099a5b47
Justin Beall <jus.beall@gmail.com>
2016-08-26 10:02:43 -0400

    Update run.py

--- wsgi/run.py
+++ wsgi/run.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index():
     return "Python - Hello World! v1.1 - Hi Candace"
 
+@app.route("/nick")
+def index():
+    return "Nick's secret route"
 
 if __name__ == "__main__":
     app.run()  # debug="True")
